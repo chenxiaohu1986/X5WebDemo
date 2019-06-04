ebec0d4c8f07f969e3a5ac3c7fe8c66dd799b7c3
chenxiaohu1986 <523940697@qq.com>
2019-06-04 17:24:43 +0800

    add

--- app/src/main/java/com/example/test_webview_demo/APPAplication.java
+++ app/src/main/java/com/example/test_webview_demo/APPAplication.java
@@ -12,7 +12,7 @@ public class APPAplication extends Application {
 		// TODO Auto-generated method stub
 		super.onCreate();
 		//搜集本地tbs内核信息并上报服务器，服务器返回结果决定使用哪个内核。
-		
+		QbSdk.setDownloadWithoutWifi(true);
 		QbSdk.PreInitCallback cb = new QbSdk.PreInitCallback() {
 			
 			@Override
